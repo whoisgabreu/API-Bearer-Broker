c6b87f58f41d990ebe042b67b2c000994b88a358
Gabreu <gabreumartins02@gmail.com>
2025-06-23 18:47:26 -0300

    a

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def extrair_bearer():
     
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000)
---
+++ teste.py
@@ -0,0 +1,13 @@
+import requests
+
+
+API_KEY = "ccf9d74a30dc58035c50d1d0cb19dd20"
+
+url = "http://31.97.27.195:5001/broker/extrair_bearer"
+header = {
+    "X-API-KEY": API_KEY
+}
+
+response = requests.get(url=url, headers=header)
+
+print(response.json())
