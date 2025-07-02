14540d7e15aa86ed20f8140d24d9248b9eff91c3
Gabreu <gabreumartins02@gmail.com>
2025-07-02 14:23:08 -0300

    Centralização dos endpoints em um unico app

--- app.py
+++ app.py
@@ -1,55 +1,18 @@
-# from flask import Flask, request, jsonify
-# from modules.login_broker import ProjetoBroker
-
-# app = Flask(__name__)
-
-# # LITERALMENTE "gabrielbucetinha123"
-# API_KEY = "ccf9d74a30dc58035c50d1d0cb19dd20"
-
-# def require_api_key(func):
-#     def wrapper(*args, **kwargs):
-#         key = request.headers.get("X-API-KEY")
-#         if key != API_KEY:
-#             return jsonify(
-#                 {"error": "Unauthorized"}
-#             ), 401
-#         return func(*args, **kwargs)
-#     wrapper.__name__ = func.__name__
-#     return wrapper
-
-
-# @app.route("/broker/extrair_bearer", methods=["GET"])
-# # @require_api_key
-# def extrair_bearer():
-
-#     try:
-        
-#         bearer = ProjetoBroker().extrair_bearer()
-
-#         return jsonify({
-#             "broker_bearer": bearer
-#         })
-    
-#     except Exception as e:
-
-#         return jsonify(
-#             {
-#                 "erro": f"{e}"
-#             }
-#         ), 400
-    
-
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000)
-
-
 from flask import Flask, request, jsonify
 from modules.login_broker import ProjetoBroker
+from modules.ferramentas_analise import GoogleTransparency
+from modules.ferramentas_analise import GoogleBusiness
+from modules.ferramentas_analise import DuckDuckGo
+from modules.cnpjaAPICustom import coletar_cnpj
+from modules.cnpja_api import search
+from dotenv import load_dotenv
+import os
 from threading import Lock
 
 app = Flask(__name__)
+load_dotenv()
 
-API_KEY = "ccf9d74a30dc58035c50d1d0cb19dd20"
+API_KEY = os.getenv("API_KEY")
 selenium_lock = Lock()  # lock global
 
 def require_api_key(func):
@@ -61,6 +24,49 @@ def require_api_key(func):
     wrapper.__name__ = func.__name__
     return wrapper
 
+@app.route("/analise/presenca-online", methods=["GET"])
+@require_api_key
+def presenca_online():
+
+    try:
+        cnpj = request.args.get("cnpj")
+        business_info = search(cnpj = cnpj)
+        business_info = GoogleTransparency().analyse(business_info)
+        # business_info = GoogleBusiness().analyse(business_info) # Reativar quanto corrigir o problema
+        business_info["paginas_online"] = []
+        business_info["paginas_online"].append(DuckDuckGo().buscar(business_info["empresa"]["nome_fantasia"], ".br"))
+        business_info["paginas_online"].append(DuckDuckGo().buscar(business_info["empresa"]["nome_fantasia"], "instagram.com"))
+        business_info["paginas_online"].append(DuckDuckGo().buscar(business_info["empresa"]["nome_fantasia"], "facebook.com"))
+
+        return jsonify(business_info)
+    
+    except Exception as e:
+
+        return jsonify(
+            {
+                "erro": f"{e}"
+            }
+        ), 400
+    
+@app.route("/analise/coletar-cnpj", methods=["GET"])
+@require_api_key
+def coletar_cnp_APIj():
+
+    try:
+        nome_socio = request.args.get("socio")
+        nome_fantasia = request.args.get("alias")
+
+        dados_empresa = coletar_cnpj(nome_socio, nome_fantasia)
+
+        return jsonify(dados_empresa)
+    
+    except Exception as e:
+
+        return jsonify(
+            {
+                "erro": f"{e}"
+            }
+        ), 400
 
 @app.route("/broker/extrair_bearer", methods=["GET"])
 @require_api_key
