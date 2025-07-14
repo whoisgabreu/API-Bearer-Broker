ecc18334efbbf8818573552cdbc2ae4b47ef105b
Gabreu <gabreumartins02@gmail.com>
2025-07-14 17:29:40 -0300

    aaaa

--- modules/login_broker.py
+++ modules/login_broker.py
@@ -13,7 +13,7 @@ class ProjetoBroker:
 
     def __init__(self):
         base_dir = os.path.join(os.path.dirname(__file__), "chrome_user_data")
-
+        print(webdriver.__version__)
 
         # Configuração do Selenium
         self.options = webdriver.ChromeOptions()
@@ -24,7 +24,7 @@ class ProjetoBroker:
         self.options.add_argument("--window-size=800,600")
 
         self.options.add_argument(f'--user-data-dir={base_dir}')
-        # self.options.add_argument(f'--profile-directory=Default')  # Isso garante que use a Default
+        self.options.add_argument(f'--profile-directory=Default')  # Isso garante que use a Default
 
     def iniciar_driver(self):
         driver = webdriver.Chrome(service=Service(), options=self.options)
